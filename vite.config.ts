846b20d537a743d2eb42408475673e3e50e5fc2b
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2025-12-21 20:31:30 +0000

    use pnpm, typescript boilerplate

---
+++ vite.config.ts
@@ -0,0 +1,91 @@
+import { defineConfig, Plugin } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import suidPlugin from "@suid/vite-plugin";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import solidSvg from 'vite-plugin-solid-svg'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const ROUTES = [
+	`/re`,
+	`/swilib`,
+	`/swilib/phone`,
+];
+
+function postBuildPlugin(): Plugin {
+	return {
+		name: 'postbuild-plugin',
+		async closeBundle() {
+			const isSymlinkExists = (file: string) => {
+				try {
+					fs.lstatSync(file);
+					return true;
+				} catch (e) {
+					return false;
+				}
+			};
+
+			let outDir = `${import.meta.dirname}/dist`;
+			for (let routeDir of ROUTES) {
+				let symlinkSrc = path.relative(`${outDir}/${routeDir}`, `${outDir}/index.html`);
+				let symlinkDst = `${outDir}/${routeDir}/index.html`;
+
+				fs.mkdirSync(`${outDir}/${routeDir}`, { recursive: true });
+
+				if (isSymlinkExists(symlinkDst))
+					fs.unlinkSync(symlinkDst);
+
+				fs.symlinkSync(symlinkSrc, `${outDir}/${routeDir}/index.html`);
+			}
+		}
+	};
+}
+
+export default defineConfig({
+	worker: {
+		format: 'es',
+		plugins: () => [ tsconfigPaths(), nodePolyfills() ]
+	},
+	plugins: [
+		suidPlugin(),
+		solidPlugin(),
+		tsconfigPaths(),
+		nodePolyfills(),
+		postBuildPlugin(),
+		solidSvg({
+			 defaultAsComponent: true,
+			 svgo: {
+				 enabled: true,
+				 svgoConfig: {
+					 plugins: [
+						 {
+							 name: 'preset-default',
+							 params: {
+								 overrides: {
+									 removeViewBox: false,
+								 },
+							 },
+						 },
+					 ],
+				 },
+			 },
+		 }),
+	],
+	optimizeDeps: {
+		exclude: [
+			'siemens-sms-parser',
+			'@sie-js/creampie',
+			'@sie-js/libffshit',
+			'@ffmpeg/util',
+			'@ffmpeg/ffmpeg',
+			'@ffmpeg/core',
+		],
+	},
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext'
+	},
+});
